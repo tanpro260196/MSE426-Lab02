2450034df7ea48263ccfe9ac47f88e493d94056f
tanpro260196 <tncao@sfu.ca>
2019-02-05 22:30:44 -0800

    Update main.m

--- main.m
+++ main.m
@@ -1,45 +1,45 @@
-%% 01
-clear;
-x10 = [0 0];
-options = optimoptions('fminunc');
-[x1, fval, exitflag, output] = fminunc(@lab021,x10,options); 
-f1_funccount = output.funcCount
-f1_itercount = output.iterations
-%% 02
-x20 = [1 1 1 1];
-[x2, fval2, exitflag2, output2] = fminunc(@lab022,x20,options); 
-f2_funccount = output2.funcCount
-f2_itercount = output2.iterations
-
-%% 03
 clear;
 clc;
-x30 = [5 5];
-options_con = optimoptions('fmincon');
-options_con.OptimalityTolerance = 1e-06;
-options_con.MaxFunctionEvaluations = 3000;
-options_con.SpecifyObjectiveGradient = 0;
-options_con.HessianApproximation = 'bfgs';
-[x3, fval3, exitflag3, output3] = fmincon(@lab023,x30,[],[],[],[],[],[],@lab023_con,options_con); 
-f3_funccount = output3.funcCount
-f3_itercount = output3.iterations
+a = 1.0;
+f1=[];
+f2=[];
+f3=[];
+f4=[];
+f5=[];
+while (a<=5)
+    %% 01
+    x10 = a(ones(1, 2));
+    options = optimoptions('fminunc');
+    [x1, fval, exitflag, output] = fminunc(@lab021,x10,options); 
+    f1 = [f1; x1 fval output.iterations output.funcCount];
+    %% 02
+    x20 = [a a a a];
+    [x2, fval2, exitflag2, output2] = fminunc(@lab022,x20,options); 
+    f2 = [f2; x2 fval2 output2.iterations output2.funcCount];
 
-%% 04
-clear;
-clc;
-x40 = [1 1];
-options_con2 = optimoptions('fmincon');
-[x4, fval4, exitflag4, output4] = fmincon(@lab024,x40,[],[],[],[],[],[],@lab024_con,options_con2); 
-f4_funccount = output4.funcCount
-f4_itercount = output4.iterations
+    %% 03
+    x30 = [1 1];
+    options_con = optimoptions('fmincon');
+    % options_con.OptimalityTolerance = 1e-6;
+    % options_con.MaxFunctionEvaluations = 40;
+    % options_con.SpecifyObjectiveGradient = false;
+    % options_con.HessianApproximation = 'ffcssad';
+     options_con.SubproblemAlgorithm = 'cg';
+    [x3, fval3, exitflag3, output3] = fmincon(@lab023,x30,[],[],[],[],[],[],@lab023_con,options_con); 
+    f3 = [f3; x3 fval3 output3.iterations output3.funcCount];
 
-%% 05
-clear;
-clc;
-x50 = [1 1 1 1 1 1];
-options_con3 = optimoptions('fmincon');
-[x5, fval5, exitflag5, output5] = fmincon(@lab025,x50,[],[],[],[],[],[],@lab025_con,options_con3); 
-f5_funccount = output5.funcCount
-f5_itercount = output5.iterations
+    %% 04
+    x40 = [a a];
+    options_con2 = optimoptions('fmincon');
+    [x4, fval4, exitflag4, output4] = fmincon(@lab024,x40,[],[],[],[],[],[],@lab024_con,options_con2); 
+    f4 = [f4; x4 fval4 output4.iterations output4.funcCount];
+
+    %% 05
+    x50 = [a a a a a a];
+    options_con3 = optimoptions('fmincon');
+    [x5, fval5, exitflag5, output5] = fmincon(@lab025,x50,[],[],[],[],[],[],@lab025_con,options_con3); 
+    f5 = [f5;x5 fval5 output5.iterations output5.funcCount];
+     a=a+1;
+end
 
 
